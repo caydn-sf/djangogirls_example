74c274f824038b0e6da352b09437f02bd0700911
cfve <crnydn@gmail.com>
2017-10-14 17:51:38 +0300

    gönderi düzenleme ve güvenlik

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post 
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.utils import timezone
 
 
 def post_list(request):
@@ -15,5 +17,31 @@ def post_detail(request, pk):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+    else:
+        form = PostForm()
+    if form.is_valid():
+            post = form.save(commit=False)
+            post.yazar = request.user
+            post.yayinlanma_tarihi = timezone.now()
+            post.save()
+            
+    
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.yazar = request.user
+            post.yayinlanma_tarihi = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
