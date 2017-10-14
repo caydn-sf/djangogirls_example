2b19fc5ff5cd8bbfa7820c38cb7f015532472ea8
cfve <crnydn@gmail.com>
2017-10-14 16:54:08 +0300

    gönderiye ayrıntı linki verdim ve 404 sayfası oluşturdum.

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
 
 	yazar= models.ForeignKey('auth.User')
 	baslik= models.CharField(max_length=200)
-	yazi= models.TextField
+	yazi= models.TextField()
 	yaratilma_tarihi= models.DateTimeField(default=timezone.now)
 	yayinlanma_tarihi= models.DateTimeField(blank=True, null=True)
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from blog.models import Post 
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts=Post.objects.all()
+
+	return render(request, 'blog/post_list.html', {'gonderiler': posts})
+
+def post_detail(request, pk):
+	posts = Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {'gonderiler': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+    
+
